c4864ced6c0be8269bd575bf56fa4b0361d63208
Jack Boehrer <jboehrer@openfox.com>
2018-04-09 11:27:28 -0500

    added 'load' method to Table class

--- table.py
+++ table.py
@@ -7,8 +7,8 @@ class Table:
     rows = None
     schema = None
 
-    def __init__(self, schema):
-        self.rows = []
+    def __init__(self, schema, rows):
+        self.rows = rows
         self.schema = schema
     def getSchema(self):
         return self.schema
@@ -29,6 +29,13 @@ class Table:
     def save(self):
         with open('data.py', 'w') as file:
             file.write(self.getJson())
+    @staticmethod
+    def load(path):
+        with open('./data.py') as data:
+            table = json.load(data)
+            schema = table['schema']
+            rows = table['rows']
+            return Table(schema, rows)
 
 class Row:
     values = None
@@ -45,31 +52,19 @@ class Row:
         return None
 
 schema = [{'column':'c1', 'type':'string'},{'column':'c2', 'type':'string'}]
-table = Table(schema)
 new_row1 = ['yay1', 'nay1']
 new_row2 = ['yay2', 'nay2']
 new_row3 = ['yay3', 'nay3']
-table.appendRow(new_row1)
-table.appendRow(new_row2)
-table.appendRow(new_row3)
-rowski = table.getRow(1)
-print(rowski.getValue('c1'))
+table = Table.load('./data')
 print(table.getJson())
-table.save()
 repo = Repo('.')
 index = repo.index
-index.add(['./data.py'])
+#index.add(['./data.py'])
 
 
-origin = repo.remotes['github']
-assert origin.exists()
-origin.fetch()                  # assure we actually have data. fetch() returns useful information
-# Setup a local tracking branch of a remote branch
-repo.create_head('master', origin.refs.master)  # create local branch "master" from remote "master"
-repo.heads.master.set_tracking_branch(origin.refs.master)  # set local "master" to track remote "master
-repo.heads.master.checkout()  # checkout local "master" to working tree
-# Three above commands in one:
-repo.create_head('master', origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+#origin = repo.remotes['github']
+#assert origin.exists()
 # push and pull behaves similarly to `git push|pull`
-origin.pull()
+#origin.pull()
+#origin.push()
 # assert not empty_repo.delete_remote(ori
